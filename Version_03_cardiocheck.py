be8aa599019468251a97943abc117fdb10ef2fce
Ninagub <161875030+Ninagub@users.noreply.github.com>
2024-04-27 17:39:50 +0200

    Update Version_03_cardiocheck.py

--- Version_03_cardiocheck.py
+++ Version_03_cardiocheck.py
@@ -163,15 +163,15 @@ def show_measurements():
         go_to_home_screen()
     st.title('Messungen')
     datum = st.date_input("Datum")
-        uhrzeit = st.time_input("Uhrzeit")
-        wert_systolisch = st.number_input("Wert Systolisch (mmHg)", min_value=0)
-        wert_diastolisch = st.number_input("Wert Diastolisch (mmHg)", min_value=0)
-        puls = st.number_input("Puls (bpm)", min_value=0)
-        kommentare = st.text_area("Kommentare")
-        submit_button = st.form_submit_button("Messungen speichern")
-        if submit_button:
-            save_measurements_to_github(datum, uhrzeit, wert_systolisch, wert_diastolisch, puls, kommentare)
-            st.success("Messungen erfolgreich gespeichert!")
+    uhrzeit = st.time_input("Uhrzeit")
+    wert_systolisch = st.number_input("Wert Systolisch (mmHg)", min_value=0)
+    wert_diastolisch = st.number_input("Wert Diastolisch (mmHg)", min_value=0)
+    puls = st.number_input("Puls (bpm)", min_value=0)
+    kommentare = st.text_area("Kommentare")
+    submit_button = st.form_submit_button("Messungen speichern")
+    if submit_button:
+        save_measurements_to_github(datum, uhrzeit, wert_systolisch, wert_diastolisch, puls, kommentare)
+        st.success("Messungen erfolgreich gespeichert!")
     
 def show_measurement_form():
     with st.form("measurement_form"):
