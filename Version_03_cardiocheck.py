54b4f639abc85219f7a60c6a4a70b3b850ea26b9
rohreali <161828529+rohreali@users.noreply.github.com>
2024-04-27 12:21:30 +0200

    Update Version_03_cardiocheck.py

--- Version_03_cardiocheck.py
+++ Version_03_cardiocheck.py
@@ -97,13 +97,17 @@ def register_user(username, password, name=None, vorname=None, geschlecht=None, 
 def verify_login(username, password):
     user_profiles = load_user_profiles()
     if username in user_profiles.index:
-        stored_hash = user_profiles.loc[username, 'password_hash']
-        # Stellen Sie sicher, dass der Hash als Bytes gelesen wird
-        if isinstance(stored_hash, str):
-            stored_hash = stored_hash.encode('utf-8')
-        if bcrypt.checkpw(password.encode('utf-8'), stored_hash):
-            st.session_state['current_user'] = username
-            return True
+        # Der gespeicherte Hash ist ein String, repräsentiert aber Bytes
+        # Entfernen der b''-Klammern und Dekodieren des Hex-Strings in Bytes
+        stored_hash_str = user_profiles.loc[username, 'password_hash']
+        if stored_hash_str.startswith("b'") and stored_hash_str.endswith("'"):
+            # Extrahiere den Inhalt des Byte-Strings
+            stored_hash_str = stored_hash_str[2:-1]
+            # Konvertiere den String zurück in Bytes
+            stored_hash = bytes(stored_hash_str, 'utf-8').decode('unicode_escape').encode('raw_unicode_escape')
+            if bcrypt.checkpw(password.encode('utf-8'), stored_hash):
+                st.session_state['current_user'] = username
+                return True
     st.error("Incorrect username or password.")
     return False
 
