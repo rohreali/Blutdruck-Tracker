f39acf4f9386a7221a7d9115c15bfd94739eedd2
Ninagub <161875030+Ninagub@users.noreply.github.com>
2024-04-27 17:55:08 +0200

    Update Version_03_cardiocheck.py

--- Version_03_cardiocheck.py
+++ Version_03_cardiocheck.py
@@ -159,21 +159,26 @@ if __name__== "_main_":
 def go_to_home_screen():
     st.session_state['page'] = 'home_screen'  
 def show_measurements():
-    if st.button('Zurück zum Home-Bildschirm'):
-        go_to_home_screen()
-    st.title('Messungen')
-    with st.form("measurement_form"):
-        datum = st.date_input("Datum")
-        uhrzeit = st.time_input("Uhrzeit")
-        wert_systolisch = st.number_input("Wert Systolisch (mmHg)", min_value=0)
-        wert_diastolisch = st.number_input("Wert Diastolisch (mmHg)", min_value=0)
-        puls = st.number_input("Puls (bpm)", min_value=0)
-        kommentare = st.text_area("Kommentare")
-        submit_button = st.form_submit_button("Messungen speichern")
+    # Sidebar für Auswahl hinzufügen
+    option = st.sidebar.selectbox("Optionen", ["Neue Messung hinzufügen", "Messhistorie anzeigen"])
+    if option == "Neue Messung hinzufügen":
+        if st.button('Zurück zum Home-Bildschirm'):
+            go_to_home_screen()
+        st.title('Messungen')
+        with st.form("measurement_form"):
+            datum = st.date_input("Datum")
+            uhrzeit = st.time_input("Uhrzeit")
+            wert_systolisch = st.number_input("Wert Systolisch (mmHg)", min_value=0)
+            wert_diastolisch = st.number_input("Wert Diastolisch (mmHg)", min_value=0)
+            puls = st.number_input("Puls (bpm)", min_value=0)
+            kommentare = st.text_area("Kommentare")
+            submit_button = st.form_submit_button("Messungen speichern")
 
         if submit_button:
             save_measurements_to_github(datum, uhrzeit, wert_systolisch, wert_diastolisch, puls, kommentare)
             st.success("Messungen erfolgreich gespeichert!")
+            
+    elif option == "Messhistorie
             
 def save_measurements_to_github(datum, uhrzeit, systolic, diastolic, pulse, comments):
     # Convert the data to a dictionary to store it in a CSV format
