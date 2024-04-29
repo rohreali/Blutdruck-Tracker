e7b946c22f2679918ca7c57aa33bd8374f64e32c
rohreali <161828529+rohreali@users.noreply.github.com>
2024-04-29 15:18:57 +0200

    Update Version_03_cardiocheck.py

--- Version_03_cardiocheck.py
+++ Version_03_cardiocheck.py
@@ -640,6 +640,7 @@ def show_emergency_numbers():
     st.write("Gespeicherte Notfallnummern:")
     emergency_data = load_emergency_numbers()
     for entry in emergency_data:
+        print(entry)
         st.write(f"- {entry['username']}: {entry['number']}")
 
     st.write("Neue Notfallnummer hinzufügen:")
