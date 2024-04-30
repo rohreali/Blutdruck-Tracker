31cf5c223b042a803d91bd7ae6c328f645ddde68
Ninagub <161875030+Ninagub@users.noreply.github.com>
2024-04-30 10:51:26 +0200

    Update Version_03_cardiocheck.py

--- Version_03_cardiocheck.py
+++ Version_03_cardiocheck.py
@@ -635,7 +635,45 @@ def show_medication_list():
                            mime='application/pdf')
     else:
             st.write("Keine Daten zum Herunterladen verfügbar.")
+def create_medication_pdf(medication_data):
+    pdf_buffer = BytesIO()
+    doc = SimpleDocTemplate(pdf_buffer, pagesize=letter)
+    elements = []
+    styles = getSampleStyleSheet()
+    
+    # Adding title
+    title = Paragraph("Medikamentenplan", styles['Title'])
+    elements.append(title)
 
+    # Preparing data for the table
+    data = [["Medikament", "Morgens", "Mittags", "Abends", "Nachts"]]
+    for index, row in medication_data.iterrows():
+        data.append([
+            row['med_name'], 
+            row['morgens'], 
+            row['mittags'], 
+            row['abends'], 
+            row['nachts']
+        ])
+
+    # Creating the table
+    t = Table(data)
+    t.setStyle(TableStyle([
+        ('BACKGROUND', (0,0), (-1,0), colors.lightgrey),
+        ('TEXTCOLOR', (0,0), (-1,0), colors.whitesmoke),
+        ('ALIGN', (0,0), (-1,-1), 'CENTER'),
+        ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0,0), (-1,0), 12),
+        ('BACKGROUND', (0,1), (-1,-1), colors.beige),
+        ('GRID', (0,0), (-1,-1), 1, colors.black),
+        ('BOX', (0,0), (-1,-1), 2, colors.black),
+    ]))
+    elements.append(t)
+    
+    # Building the PDF
+    doc.build(elements)
+    pdf_buffer.seek(0)
+    return pdf_buffer
 
 #hier kommt Fitness        
 def back_to_home():
