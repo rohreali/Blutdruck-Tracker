f6887f8649225c3726713b12ac67720a23ebace5
rohreali <161828529+rohreali@users.noreply.github.com>
2024-05-21 10:18:42 +0200

    Update Version_05_cardiocheck.py

--- Code/Version_05_cardiocheck.py
+++ Code/Version_05_cardiocheck.py
@@ -1092,7 +1092,7 @@ def show_info_pages():
 
     # Funktion zum Lesen des Textes aus der Datei
     def read_text_from_file(filename):
-         base_path = os.path.dirname(__file__)  # Basispfad für relative Pfade
+        base_path = os.path.dirname(__file__)  # Basispfad für relative Pfade
         filepath = os.path.join(base_path, "Code", filename)  # Pfad zur Datei
         encodings = ['utf-8', 'ISO-8859-1']  # Verschiedene Zeichenformate ausprobieren
         for encoding in encodings:
